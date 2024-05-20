ec27bef301d901a8cec721822a95c3851a471103
amanuelgthn <amanuelgthn@gmail.com>
2024-05-20 20:05:00 +0200

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,6 +6,6 @@ const readline = require('readline').createInterface({
 readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
 });
-process.on('exit', (code) => {
+process.on('exit', () => {
   console.log('This important software is now closing');
 });
